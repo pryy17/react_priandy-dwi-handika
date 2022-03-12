b984b1a5811161385777f9a284d6665eb68e173c
pryy <priandycoy@gmail.com>
2022-03-12 19:03:13 +0700

    Clean Code

--- 9_Clean Code/praktikum/script.js
+++ 9_Clean Code/praktikum/script.js
@@ -24,17 +24,17 @@ class userservice {
     // baris 7, penamaan class "userservice{}" seharusnya di awali huruf besar atau dengan PascalCase contoh "UserCase{}".
     // baris 10, penamaan function dan parameter sebaiknya menggunakan camelCase
     // baris 14 dan 15, penamaan function dan parameter sebaiknya menggunakan camelCase contoh getUserById(userId).
+    // baris 2,3,4 sebaiknya di buat 1 baris saja dengan 1 deklarasi var;
 
 // tuliskan alasan dari setiap kekurangan tersebut
     // baris 1 dan 7 menggunakan penuh huruf kecil pada penamaan class, jarang di gunakan oleh programmer untuk menamai class, karna agak sulit membedakan class dengan function
     // baris 10,14,15, menggunakan penuh huruf kecil pada penamaan function dan parameter, membuat penamaan agak sulit di fahami karna harus menejermahkan spasi antar kata.
-
+    // baris 2,3,4 sebaiknya di buat 1 baris saja dengan 1 deklarasi var agar lebih rapi dan simple
 
 //=============== problem 2 - Rewrite ===================
 // hasil Rewrite clean Code
 class Kendaraan {
-    var totalRoda = 0;
-    var kecepatanPerjam = 0;
+    var totalRoda, kecepatanPerjam;
 }
 
 class Mobil extends Kendaraan {
